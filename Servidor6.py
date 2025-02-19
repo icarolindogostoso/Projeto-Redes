946db950e693dfe95917ccefffda3b12eeb72305
icaro <164355795+icarolindogostoso@users.noreply.github.com>
2025-02-19 11:55:08 -0300

    Add files via upload

---
+++ Servidor6.py
@@ -0,0 +1,85 @@
+import socket
+from threading import Thread
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import unpad
+import time
+
+class Servidor:
+    def __init__ (self, ip: str, porta: int) -> None:
+        """Inicialização do servidor com IP e porta"""
+        self.soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # ipv4 e tcp
+        self.ipPorta = (ip, porta)
+        self.soc.bind(self.ipPorta)
+
+    def decrypt(self, iv, enc_text):
+        key = b'0361231230000000'
+        cipher = AES.new(key, AES.MODE_CBC, iv=iv)
+        decrypted_padded_message = cipher.decrypt(enc_text)
+        decrypted_message = unpad(decrypted_padded_message, AES.block_size)
+        return decrypted_message.decode("UTF-8")
+
+    def client (self, conexao, docliente):
+        """Lidar com a conexão de cada cliente"""
+        print(f"O cliente {docliente} se conectou")
+
+        testa_mensagem = ''
+
+        while True: # loop para receber mensagnes do cliente
+            mensagem_recebida = conexao.recv(1024).decode("utf-8")
+
+            if not mensagem_recebida:
+                break
+
+            if testa_mensagem != mensagem_recebida:
+                try:
+                    print("Recebi =", mensagem_recebida, ", do cliente", docliente)
+
+                    tupla = eval(mensagem_recebida)
+                    iv = tupla[0]
+                    enc_text = tupla[1]
+                    dec_text = self.decrypt(iv, enc_text)
+                    print(dec_text, "\n")
+                    # print("Decifrada =", dec_text, ", do cliente", docliente, "\n")
+                    # testa_mensagem = dec_text 
+                except Exception as e:
+                    print(e)
+
+        conexao.close()
+
+    def broadcast_server_ip(self):
+        interfaces = socket.getaddrinfo(host=socket.gethostname(), port=None, family=socket.AF_INET)
+        allips = [ip[-1][0] for ip in interfaces]
+        msg = str(self.ipPorta).encode("utf-8")
+        while True:
+            for ip in allips:
+                print(f'Publicando em {ip}')
+                sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)  # UDP
+                sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+                sock.bind((ip,0))
+                sock.sendto(msg, ("255.255.255.255", 5005))
+                sock.close()
+            time.sleep(15)
+
+    def ligar(self) -> None:
+        """Ligar o servidor e aceitar conexões
+        
+        a ideia por tras é que ele vai fazer treads simultaneas para que mais de uma pessoa
+        possa se conectar ao mesmo tempo."""
+
+        self.soc.listen(5) # numero maximo de conexoes pendentes
+
+        broadcast_thread = Thread(target=self.broadcast_server_ip, daemon=True)
+        broadcast_thread.start()
+
+        while True: # loop principal para aceitar multiplas conexoes
+            conexao, docliente = self.soc.accept() # aceita uma nova conexao
+
+            thread_cliente = Thread(target=self.client, args=(conexao, docliente)) # cria uma nova thread para tratar a conexao do cliente
+            thread_cliente.start()
+
+def main():
+    servidor = Servidor("0.0.0.0", 8000)
+    servidor.ligar()
+
+if __name__ == "__main__":
+    main()
